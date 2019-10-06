91432e7afb8644e03eca17dcd120dfd09570df91
TrizlyBear <tristanschmidt2004@gmail.com>
2019-10-06 23:10:53 +0200

    Basics work

---
+++ index.js
@@ -0,0 +1,56 @@
+const djs = require('discord.js')
+const fs = require('fs');
+
+exports.login = function(prefix, token, cmds) {
+    if (!token || !typeof token == "string") {
+        console.error(`Make sure to fill in the token parameter and make sure it's a string.`)
+        process.exit()
+    }
+
+    if (!cmds || !Array.isArray(cmds)) {
+        console.error(`Make sure to fill in the commands parameter and make sure it's an array.`)
+        process.exit()
+    }
+
+    if (!prefix || !typeof prefix == "string") {
+        console.error(`Make sure to fill in the prefix parameter and make sure it's an string.`)
+        process.exit()
+    }
+    
+    const client = new djs.Client();
+
+    client.commands = new djs.Collection();
+
+    const commandFiles = fs.readdirSync('./node_modules/jscord/commands').filter(file => file.endsWith('.js'));
+
+    for (const file of commandFiles) {
+        const command = require(`./commands/${file}`);
+        client.commands.set(command.name, command);
+    }
+
+    client.on('message', message => {
+        if (!message.content.startsWith(prefix) || message.author.bot) return;
+    
+        const args = message.content.slice(prefix.length).split(/ +/);
+        const command = args.shift().toLowerCase();
+    
+        if (!client.commands.has(command)) return;
+    
+        try {
+            client.commands.get(command).execute(message, message, args);
+        } catch (error) {
+            console.error(error);
+            message.reply('there was an error trying to execute that command!');
+        }
+    });
+
+    client.once('ready', () => {
+        console.log('Ready!');
+    });
+
+    client.login(token)
+}
+
+exports.cmd = function(command, text) {
+
+}
